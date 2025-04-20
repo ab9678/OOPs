2a53cbe71fe669c71cf575c05c4b1a6d079b8fe6
Abhigyan <abhigyanbhattacharya75@gmail.com>
2025-04-20 23:21:06 +0530

    Update

--- labFile/L2/functionOverloading.cpp
+++ labFile/L2/functionOverloading.cpp
@@ -1,6 +1,22 @@
 # include <iostream>
 using namespace std;
 
-int main(){
+
+class Printer {
+public:
+    void print(int i) {
+        cout << "Printing int: " << i << endl;
+    }
+
+    void print(double d) {
+        cout << "Printing double: " << d << endl;
+    }
+
     
+};
+
+int main() {
+    Printer p1;
+    p1.print(23);
+    p1.print(23.2);
 }
