7e3e1a07d60cfb60ef85d592ffffa2cebd57ea13
Abhigyan <abhigyanbhattacharya75@gmail.com>
2025-06-02 00:28:00 +0530

    New

---
+++ practice/exam/file.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+using namespace std;
+# include <fstream>
+
+
+int main(){
+    ofstream wfile;
+    ifstream rfile;
+
+    wfile.open("Hello.txt");
+    wfile<<"Yo guys whats up!";
+
+    wfile.close();
+
+    rfile.open("Hello.txt");
+
+    string s;
+    while(!rfile.eof()){
+        s = rfile.get();
+        cout<<s;
+    }
+}
